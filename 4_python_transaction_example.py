a90f65cad0e19d5bf19d43b422fac50ea099baf3
gilor19 <gilor19@gmail.com>
2022-11-17 12:23:19 +0200

    Add transaction code example

---
+++ 4_python_transaction_example.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+### ATTENTION! Running this code will make changes on your Sakila dataset. Please be careful ###
+
+# Create a connection
+cnx = mysql.connector.connect(
+    user='username',
+    password='password',
+    host='127.0.0.1',
+    database='sakila'
+)
+
+# Create a cursor
+cursor = cnx.cursor()
+
+try:
+    # Execute the SQL command
+    cursor.execute('UPDATE film SET rental_rate = rental_rate + rental_rate*0.2')
+    cnx.commit()
+
+except:
+    # Rollback in case there is any error
+    cnx.rollback()
+
+# Disconnect from server
+cnx.close()
